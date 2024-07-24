37c04c0e83426ee0df71b76efa16b0f365821956
Ben Ho <bennyhch@gmail.com>
2024-07-24 09:37:08 +0100

    feat: add sorting logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,30 @@
-'use client'
+"use client";
 import { useGalleryData } from "@/lib/useGalleryData";
+import { useState } from "react";
+import ImageCardContainer from "./ImageCardContainer";
 
 export default function Home() {
+  const { data } = useGalleryData();
+
+  const [sortedNumbers, setSortedNumbers] = useState(data);
+  const [toggleOrder, setToggleOrder] = useState(false);
+
+  const handleSort = () => {
+    const newOrder = [...sortedNumbers].reverse();
+    setSortedNumbers(newOrder);
+    setToggleOrder(!toggleOrder);
+  };
+
   return (
     <main>
-      Add content and style according to https://www.figma.com/design/cqwIYXNHitVg2nAlMBfObA/Web-Developer-Test?node-id=0-1&t=nQJuZMDmryzLddLl-1
+      <h1 className="text-2xl font-bold mb-4">Web Developer Gallery</h1>
+      {/* <ImageCardContainer numbers={sortedNumbers} /> */}
+      {sortedNumbers.map((image, index) => (
+        <div>
+          <img src={`img/${image.filename}`} alt="" />
+        </div>
+      ))}
+      <button onClick={handleSort}>Sort</button>
     </main>
   );
 }
